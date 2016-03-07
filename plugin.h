ff122b5cc5ca93233e64124329c2e9d550ed7a6e
Shi Quan <shiquan.cn@gmail.com>
2016-03-07 22:53:34 +0800

    modified:   plugin.h

--- plugin.h
+++ plugin.h
@@ -1,6 +1,8 @@
 #ifndef VCFANNO_PLUGIN_HEADER
 #define VCFANNO_PLUGIN_HEADER
 
+/* try to convert any data from SQL into annot_line struct and annotated by core_annotate()
+ */
 typedef struct {
     char **cols;
     int ncols, mcols;
