2efd91d0efa54a4a5e32aa6809e6f411fded9ff7
Shi Quan <shiquan.cn@gmail.com>
2016-03-14 21:34:35 +0800

    modified:   anno.h 	modified:   anno_core.c

--- anno.h
+++ anno.h
@@ -113,21 +113,16 @@ extern void safe_release(void const * p, rel_func func);
 	fprintf(stderr, "[debug] func:%s, line:%d, errno:%s. " line "\n", __FUNCTION__, __LINE__, str_errno(), ##__VA_ARGS__); \
     } while(0)
 
-struct anno_option
-{
-    htsFile *out_fn;
-    int output_type, n_threads;
-};
 
 typedef struct annot_col annot_col_t;
 
-/* struct annot_cols_pack */
-/* { */
-/*     annot_col_t *cols; */
-/*     int ncols; */
-/*     int type; */
-/*     char *columns; */
-/* }; */
+struct annot_cols_pack
+{
+    annot_col_t *cols;
+    int ncols;
+    int type;
+    char *columns;
+};
 
 typedef struct
 {
@@ -140,102 +135,6 @@ struct region
     region1_t *regs;
     int n, m, c;
 };
-
-struct sql_connect
-{
-    void *connect;
-    int idx; // index id
-    char *column;
-    int ncols;
-    annot_col_t *cols;
-    int nbuffers, mbuffer;
-    annot_line_t *buffers;
-    /* kstring_t line; */
-    /* char *fname; */
-    /* char **als; // parsed alleles  */
-    /* kstring_t als_str; */
-    /* int nals, mals;   */
-    /* int als_type;  // alleles type, VCF_SNP or VCF_INDEL */
-    
-    struct region *regs; // in memory cached regions
-    /* void *seq_hash; */
-    /* char **seq_names; */
-    /* int nseqs; */
-    int iseq; // current position: chr name index to names[]
-    int start, end;
-    int prev_seq, prev_start;
-};
-
-struct sql_connect_readers
-{
-    struct sql_connect *connects;
-    int nreaders;
-};
-/* anno_handler is adapt form args_t struct in vcfannotation.c */
-struct anno_handler
-{
-    int vcf_anno_count;
-    int sql_anno_count;
-    bcf_srs_t *files;
-    bcf_hdr_t *hdr, *hdr_out;
-    struct annot_cols_pack *cols;
-    struct sql_connect_readers *connects;
-    /* filter tag, usually in FORMAT field */
-    filter_t *filter;
-    char *filter_str;
-    int filter_logic;
-
-    vcmp_t *vcmp;  // for matching annotation and VCF lines by allele
-    //annot_line_t *alines; // buffered annotation lines
-    //int nalines, malines;
-    //int ref_idx, alt_idx, from_idx, to_idx; // -1 for not present
-    //struct annot_col *cols;
-    //int ncols;
-    int mtmpi, mtmpf, mtmps;
-    int mtmpi2, mtmpf2, mtmps2;
-    int mtmpi3, mtmpf3, mtmps3;
-    int32_t *tmpi, *tmpi2, *tmpi3;
-    float *tmpf, *tmpf2, *tmpf3;
-    char *tmps, *tmps2, **tmpp, **tmpp2;
-    kstring_t tmpks;
-};
-
-struct annot_col
-{
-    int icol, replace, number;  // number: one of BCF_VL_* types
-    char *hdr_key;
-    int (*setter)(struct anno_handler *, bcf1_t *, annot_col_t *, void*);
-};
-
-extern int setter_filter(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int vcf_setter_filter(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int setter_id(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int vcf_setter_id(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int setter_qual(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int vcf_setter_qual(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int setter_info_flag(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int vcf_setter_info_flag(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int setter_ARinfo_int32(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, int nals, char **als, int ntmpi);
-extern int setter_info_int(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int vcf_setter_info_int(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int setter_ARinfo_real(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, int nals, char **als, int ntmpf);
-extern int setter_info_real(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int vcf_setter_info_real(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int copy_string_field(char *src, int isrc, int src_len, kstring_t *dst, int idst);
-extern int setter_ARinfo_string(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, int nals, char **als);
-extern int setter_info_str(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int vcf_setter_info_str(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int vcf_setter_format_gt(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int count_vals(annot_line_t *tab, int icol_beg, int icol_end);
-extern int setter_format_int(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int setter_format_real(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int setter_format_str(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int vcf_setter_format_int(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int vcf_setter_format_real(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-extern int vcf_setter_format_str(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
-
-extern void handler_release(void *hand);
-extern void rebuild_anno_lines(struct anno_handler *hand);
 
 /*
  * local HGVS nomenclature convert,
@@ -312,6 +211,106 @@ extern int local_setter_hgvs_func(struct anno_handler *hand, bcf1_t *line, annot
 extern int local_setter_hgvs_names(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
 
 extern void init_columns(anno_cols_t *cols, char *string, bcf_hdr_t *header);
+
+
+struct sql_connect
+{
+    void *connect;
+    int idx; // index id
+    char *column;
+    int ncols;
+    annot_col_t *cols;
+    int nbuffers, mbuffer;
+    annot_line_t *buffers;
+    /* kstring_t line; */
+    /* char *fname; */
+    /* char **als; // parsed alleles  */
+    /* kstring_t als_str; */
+    /* int nals, mals;   */
+    /* int als_type;  // alleles type, VCF_SNP or VCF_INDEL */
+    
+    struct region *regs; // in memory cached regions
+    /* void *seq_hash; */
+    /* char **seq_names; */
+    /* int nseqs; */
+    int iseq; // current position: chr name index to names[]
+    int start, end;
+    int prev_seq, prev_start;
+};
+
+/* struct sql_connect_readers */
+/* { */
+/*     struct sql_connect *connects; */
+/*     int nreaders; */
+/* }; */
+/* anno_handler is adapt form args_t struct in vcfannotation.c */
+struct anno_handler
+{
+    int vcf_anno_count;
+    int sql_anno_count;
+    bcf_hdr_t *hdr, *hdr_out;
+    htsFile *out_fn;
+    int output_type, n_threads;
+    bcf_srs_t *files;
+    struct annot_cols_pack *cols;
+    struct sql_connect *connects;
+
+    /* filter tag, usually in FORMAT field */
+    filter_t *filter;
+    char *filter_str;
+    int filter_logic;
+
+    vcmp_t *vcmp;  // for matching annotation and VCF lines by allele
+    //annot_line_t *alines; // buffered annotation lines
+    //int nalines, malines;
+    //int ref_idx, alt_idx, from_idx, to_idx; // -1 for not present
+    //struct annot_col *cols;
+    //int ncols;
+    int mtmpi, mtmpf, mtmps;
+    int mtmpi2, mtmpf2, mtmps2;
+    int mtmpi3, mtmpf3, mtmps3;
+    int32_t *tmpi, *tmpi2, *tmpi3;
+    float *tmpf, *tmpf2, *tmpf3;
+    char *tmps, *tmps2, **tmpp, **tmpp2;
+    kstring_t tmpks;
+};
+
+struct annot_col
+{
+    int icol, replace, number;  // number: one of BCF_VL_* types
+    char *hdr_key;
+    int (*setter)(struct anno_handler *, bcf1_t *, annot_col_t *, void*);
+};
+
+extern int setter_filter(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int vcf_setter_filter(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int setter_id(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int vcf_setter_id(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int setter_qual(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int vcf_setter_qual(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int setter_info_flag(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int vcf_setter_info_flag(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int setter_ARinfo_int32(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, int nals, char **als, int ntmpi);
+extern int setter_info_int(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int vcf_setter_info_int(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int setter_ARinfo_real(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, int nals, char **als, int ntmpf);
+extern int setter_info_real(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int vcf_setter_info_real(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int copy_string_field(char *src, int isrc, int src_len, kstring_t *dst, int idst);
+extern int setter_ARinfo_string(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, int nals, char **als);
+extern int setter_info_str(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int vcf_setter_info_str(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int vcf_setter_format_gt(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int count_vals(annot_line_t *tab, int icol_beg, int icol_end);
+extern int setter_format_int(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int setter_format_real(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int setter_format_str(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int vcf_setter_format_int(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int vcf_setter_format_real(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+extern int vcf_setter_format_str(struct anno_handler *hand, bcf1_t *line, annot_col_t *col, void *data);
+
+extern void handler_release(void *hand);
+extern void rebuild_anno_lines(struct anno_handler *hand);
 
 
 #endif
--- anno_core.c
+++ anno_core.c
@@ -1,6 +1,17 @@
 #include "anno.h"
 #include "plugin.h"
 
+struct {
+    uint64_t all_sites;
+    uint64_t refs;
+    uint64_t snps;
+    uint64_t mnps;
+    uint64_t ti;
+    uint64_t tv;
+    uint64_t indels;
+    uint64_t other;
+} sites_stat = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
 //extern struct configs anno_config_file;
 
 struct anno_handler hand = { .files=NULL, .hdr=NULL, .hdr_out=NULL, .sql_anno_count=0, .vcf_anno_count=0, .connects=NULL };
@@ -33,10 +44,30 @@ void init_data(const char *json, const char *fname)
 void init_buffers(int start, int end_pos)
 {
 }
+
+#define VARSTAT(x) do \
+    {				\
+	sites_stat.all_sites++; \
+	if (x == VCF_REF) { \
+	    sites_stat.refs++;\
+	} else if (x == VCF_SNP) {\
+	    sites_stat.snps++;\
+	} else if (x == VCF_MNP) {\
+	    sites_stat.mnps++;\
+	} else if (x == VCF_INDEL) {\
+	    sites_stat.indels++;\
+	} else if (x == VCF_OTHER) {\
+	    sites_stat.other++;\
+	} else {\
+	    error("VARSTAT: Unknown flag %d", x);\
+	}\
+    } while(0)
+	    
 bcf1_t * anno_core(bcf1_t *line)
 {
     int i, j;
-    bcf_get_variant_types(line); // get the variant type
+    int x = bcf_get_variant_types(line); // get the variant type
+    VARSTAT(x);
     // get the start position of variant
     int len = 0;
     for (i=1; i<line->n_allele; i++) {
@@ -44,13 +75,52 @@ bcf1_t * anno_core(bcf1_t *line)
 	    len = line->d.var[i].n;
 	}
     }
+    
     int end_pos = len<0 ? line->pos - len: line->pos;
     init_buffers(line->pos, end_pos);
-    
+    for (i=0; i<hand->sql_anno_count; ++i) {
+	anno_sql_line(hand->connects[i], line);
+    }
+    for (i=0; i<hand->vcf_anno_count; ++i) {
+	anno_vcf_line(i, line);
+    }
+    return line;
+}
+#undef VARSTAT
+
+static char *input_fname = NULL;
+static char *config_fname = NULL;
+
+int paras_praser(int argc, char **argv)
+{
+
+    assert(input_fname != NULL);
+    htsFile *fp = hts_open(input_fname, "r");
+    htsFormat type = *hts_get_format(fp);
+    hts_close(fp);
+    if (type.format!=vcf && type.format!=bcf) {
+	error("Unsupport input format! %s", input_fname);
+    }
+    return 0;
 }
 
 int main(int argc, char **argv)
 {
-    
+    if ( para_praser(argc, argv) ) {
+	return EXIT_FAILURE;
+    }
+    check_environ_paras();
+    init_data(config_fname, input_fname);
+    while( bcf_sr_next_line(hand->files))
+    {
+	if ( !bcf_sr_has_line(hand->files, 0) ) continue;
+	bcf1_t *line = bcf_sr_get_line(hand->files, 0);
+	if ( line->errcode ) {
+	    error("Encountered error, cannot proceed. Please check the error output above.\n");
+	}
+	anno_core(line);
+	bcf_write1(hand->out_fh, hand->hdr_out, line);
+    }
+    release_handler();
     return EXIT_SUCCESS;
 }
