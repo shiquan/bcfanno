0986d87a1ca2588e9504d2415cfb16ab29ead1ea
Shi Quan <shiquan.cn@gmail.com>
2016-04-22 01:23:51 +0800

    new file:   hgvslib/hgvs.c

---
+++ hgvslib/hgvs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <htslib/faidx.h>
+#include <htslib/vcf.h>
+#include "hgvs.h"
+
+struct refgene_mempools pool = {
+    n=0, m=0,
+    entrys = NULL,
+    begin = -1,
+    end =-1,
+    tid=-1,
+    lastbegin = -1,
+    lastend =-1
+};
+
+struct hgvs *pos_anno_hgvs(bcf1_t *line)
+{
+    int end = line->pos+line->rlen-1;
+    
+}
+
