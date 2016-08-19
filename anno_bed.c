8060b9df62776a59a96ade99668307dab62d2fca
Shi Quan <shiquan.cn@gmail.com>
2016-08-19 13:58:13 +0800

    new file:   anno_bed.c

---
+++ anno_bed.c
@@ -0,0 +1,12 @@
+#include "anno.h"
+#include "anno_bed.h"
+
+#ifdef _BED_ANNO_MAIN
+int main(int argc, char **argv)
+{
+    prase_args(argc, argv);
+
+    // read line
+    // annotate function region
+}
+#endif
